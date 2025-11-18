271cb35ff5e4c95b2633c16074b63d618cd5c886
sofiaartom <s342549@studenti.polito.it>
2025-11-18 12:29:19 +0100

    programma terminato

--- model/impianto_DTO.py
+++ model/impianto_DTO.py
@@ -17,6 +17,8 @@ class Impianto:
     def get_consumi(self):
         """ Aggiorna e Restituisce la lista di consumi (self.lista_consumi) associati all'impianto"""
         # TODO
+        self.lista_consumi = ConsumoDAO().get_consumi(self.id)
+        return self.lista_consumi
 
     def __eq__(self, other):
         return isinstance(other, Impianto) and self.id == other.id
@@ -26,4 +28,3 @@ class Impianto:
 
     def __repr__(self):
         return f"{self.id} | {self.nome} | Indirizzo: {self.indirizzo}"
-
--- model/model.py
+++ model/model.py
@@ -1,4 +1,7 @@
+from copy import deepcopy
+
 from database.impianto_DAO import ImpiantoDAO
+from model.impianto_DTO import Impianto
 
 '''
     MODELLO:
@@ -26,6 +29,18 @@ class Model:
         :return: lista di tuple --> (nome dell'impianto, media), es. (Impianto A, 123)
         """
         # TODO
+        risultati = []
+        for impianto in self._impianti:
+            somma = 0
+            counter = 0
+            consumi = impianto.get_consumi()   # metto i consumi in una lista
+            for consumo in consumi:
+                if consumo.data.month == mese:    # se il mese è quello richiesto faccio la media
+                    somma += consumo.kwh
+                    counter += 1
+            media = somma/counter
+            risultati.append((impianto.nome, media))
+        return risultati
 
     def get_sequenza_ottima(self, mese:int):
         """
@@ -47,6 +62,20 @@ class Model:
     def __ricorsione(self, sequenza_parziale, giorno, ultimo_impianto, costo_corrente, consumi_settimana):
         """ Implementa la ricorsione """
         # TODO
+        if giorno >7: # quando il giorno è più grande di 7 non ricorro più
+            # se il costo ottimo non è ancora stato cambiato o se è più grande di quello già presente lo cambio e cambia la sequenza
+            if self.__costo_ottimo == -1 or self.__costo_ottimo > costo_corrente:
+                self.__costo_ottimo = costo_corrente
+                self.__sequenza_ottima = deepcopy(sequenza_parziale)
+            return
+        for impianto, consumi in consumi_settimana.items():   # itero tra chiavi e valori(lista)
+            nuovo_costo = costo_corrente + consumi[giorno-1]   # aggiorno il costo totale con quella del giorno attuale
+            nuova_sequenza = sequenza_parziale + [impianto]   # aggiungo l'impianto di oggi alla sequenza
+            if ultimo_impianto != impianto and ultimo_impianto is not None:
+                nuovo_costo +=5   # se non sono al prim giorno e se ho cambiato impianto aggiungo 5 euro
+            self.__ricorsione(nuova_sequenza, giorno+1, impianto, nuovo_costo, consumi_settimana)  # chiamo la ricorsione con nuovi valori
+
+
 
     def __get_consumi_prima_settimana_mese(self, mese: int):
         """
@@ -54,4 +83,15 @@ class Model:
         :return: un dizionario: {id_impianto: [kwh_giorno1, ..., kwh_giorno7]}
         """
         # TODO
-
+        consumi_dict = {}
+        for impianto in self._impianti:
+            consumi = impianto.get_consumi()   # metto i consumi in una lista
+            consumo_settimana = []  # lista per ogni impianto
+            for giorno in range(1,8):
+                kw_ora = 0  # variabile per azzerare ogni volta il valore
+                for consumo in consumi:
+                    if consumo.data.month == mese and consumo.data.day == giorno:   # se il mese è giusto e finché sono tra 1 e 7
+                        kw_ora = consumo.kwh
+                consumo_settimana.append(kw_ora)
+            consumi_dict[impianto.id] = consumo_settimana  # dizionario con id:lista consumi settimana
+        return consumi_dict
